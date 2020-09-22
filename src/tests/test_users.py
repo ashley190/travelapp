1d903bf84bbb45b8fb112479c46a8ec86bb2da47
Ashley Lam <ashley_850819@yahoo.com>
2020-09-22 17:48:41 +1000

    updated test_users.py

--- src/tests/test_users.py
+++ src/tests/test_users.py
@@ -4,12 +4,11 @@ from unittest.mock import patch
 
 
 class TestUserClass(unittest.TestCase):
-    @patch('builtins.input', lambda *args: "testuser1")
     def test_user_instantiation(self):
+        @patch('builtins.input', lambda *args: "testuser1")
         testuser = User()
         self.assertEqual(testuser.name, "testuser1", msg="name instantiation error")
 
-    @patch('builtins.input', lambda *args: "Groot")
-    def test_user_instantiation(self):
+        @patch('builtins.input', lambda *args: "Groot")
         testuser = User()
         self.assertEqual(testuser.name, "Groot", msg="name instantiation error")
